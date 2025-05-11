547381dc20526b717422f2d3746714a99307e628
chackochii <edwinchacko2001@gmail.com>
2025-05-11 19:25:13 +0530

    seo friendly

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,17 +1,19 @@
 const config = {
   title: "Edwin chacko | Full Stack Developer",
   description: {
-    long: "Explore the portfolio of Abhijit, a Computer Science Engineer with 1.5 year experience in Devlopment, DevOps & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    long: "Explore the portfolio of edwin, a Computer Science Engineer with 1.5 year experience in Devlopment, web design & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
     short:
-      "Discover the portfolio of Abhijit, a DevOps enthusiast solving real life problems in DevOps and Development.",
+      "Discover the portfolio of edwin, a computer  enthusiast solving real life problems in Development.",
   },
   keywords: [
     "Edwin chacko",
-    "Edwin chacko",
+    "Edwin Chacko",
+    "edwinchacko",
+    "EDWINCHACKO",
     "Edwinchacko",
     "Edwin chacko portfolio",
     "portfolio",
-    "DevOps",
+    "Node.js",
     "Devlopment",
     "Database",
     "AI Docker file optimizer",
@@ -25,13 +27,13 @@ const config = {
     // "Spline",
     // "Framer Motion",
     "AWS",
-    "DevSecOps",
-    "AIOps",
-    "Azure",
+    "EC2",
+    "Expess.js",
+    "postgres",
   ],
   author: "Edwin chacko",
   email: "edwinchacko2001@gmail.com",
-  site: "https://.com",
+  site: "https:/edwin-chacko-portfolio.vercel.app",
 
   get ogImg() {
     return this.site + "/assets/seo/og-image.png";
